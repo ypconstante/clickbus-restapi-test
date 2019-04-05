767cf96d63a665b4b49ae36d4f88990a8ee413b6
Yuri Pereira <ypconstante@gmail.com>
2019-04-04 21:04:17 -0300

    Change ids in database to integer

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,55 +1,60 @@
 create table CLIENT_APPLICATION
 (
-    ID          serial primary key,
+    ID          integer not null auto_increment unique,
     NAME        varchar(100),
     PUBLIC_NAME varchar(255),
     CREATED_AT  datetime,
-    UPDATED_AT  datetime
+    UPDATED_AT  datetime,
+    primary key (ID)
 );
 
 create table COUNTRY
 (
-    ID         serial primary key,
+    ID         integer not null auto_increment unique,
     NAME       varchar(100),
     CREATED_AT datetime,
-    UPDATED_AT datetime
+    UPDATED_AT datetime,
+    primary key (ID)
 );
 
 create table STATE
 (
-    ID         serial primary key,
-    COUNTRY_ID long references COUNTRY (ID),
+    ID         integer not null auto_increment unique,
+    COUNTRY_ID integer references COUNTRY (ID),
     NAME       varchar(100),
     CREATED_AT datetime,
-    UPDATED_AT datetime
+    UPDATED_AT datetime,
+    primary key (ID)
 );
 
 create table CITY
 (
-    ID         serial primary key,
-    STATE_ID   long references STATE (ID),
+    ID         integer not null auto_increment unique,
+    STATE_ID   integer references STATE (ID),
     NAME       varchar(100),
     CREATED_AT datetime,
-    UPDATED_AT datetime
+    UPDATED_AT datetime,
+    primary key (ID)
 );
 
 
 create table PLACE
 (
-    ID            serial primary key,
-    CITY_ID       long references CITY (ID),
+    ID            integer not null auto_increment unique,
+    CITY_ID       integer references CITY (ID),
     NAME          varchar(255),
     TERMINAL_NAME varchar(255),
     SLUG          varchar(255),
     ADDRESS       varchar(255),
     CREATED_AT    datetime,
     UPDATED_AT    datetime,
+    primary key (ID),
     constraint PLACE_SLUG_AK unique (SLUG)
 );
 
 create table PLACE_CLIENT_APPLICATION
 (
-    PLACE_ID  long references PLACE (ID),
-    CLIENT_ID long references CLIENT_APPLICATION (ID),
+    PLACE_ID  integer references PLACE (ID),
+    CLIENT_ID integer references CLIENT_APPLICATION (ID),
     primary key (PLACE_ID, CLIENT_ID)
 )
