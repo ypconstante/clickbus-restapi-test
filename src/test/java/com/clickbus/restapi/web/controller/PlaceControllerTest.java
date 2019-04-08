c7f4c4b5680d487555bf8ebb2733e50855787301
Yuri Pereira <ypconstante@gmail.com>
2019-04-07 23:42:26 -0300

    Remove unnecessary injection in place test

--- src/test/java/com/clickbus/restapi/web/controller/PlaceControllerTest.java
+++ src/test/java/com/clickbus/restapi/web/controller/PlaceControllerTest.java
@@ -5,14 +5,12 @@ import com.clickbus.restapi.web.dto.PlaceDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PlaceControllerTest extends AppControllerTestBootstrapper {
-    @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper objectMapper;
 
     @Test
