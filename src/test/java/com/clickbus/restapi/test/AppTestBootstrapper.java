85b4c8f0580fb7432419d0bfabe0bec0154f6a1e
Yuri Pereira <ypconstante@gmail.com>
2019-04-07 21:00:05 -0300

    Extract repository test bootstrap to generic class
    Spring boot setup will be used to setup controller tests.

---
+++ src/test/java/com/clickbus/restapi/test/AppTestBootstrapper.java
@@ -0,0 +1,15 @@
+package com.clickbus.restapi.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
+@TestPropertySource(locations = {
+    "classpath:application.properties",
+    "classpath:test.properties"
+})
+abstract class AppTestBootstrapper extends SpringBootTestContextBootstrapper {
+}
