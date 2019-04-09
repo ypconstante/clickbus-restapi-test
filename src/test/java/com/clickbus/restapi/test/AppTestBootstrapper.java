39d86242202cbea0bff88a86df4422ab5ca4e7f5
Yuri Pereira <ypconstante@gmail.com>
2019-04-09 06:08:22 -0300

    Change test properties to use profile

--- src/test/java/com/clickbus/restapi/test/AppTestBootstrapper.java
+++ src/test/java/com/clickbus/restapi/test/AppTestBootstrapper.java
@@ -3,15 +3,12 @@ package com.clickbus.restapi.test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-@TestPropertySource(locations = {
-    "classpath:application.properties",
-    "classpath:test.properties"
-})
+@ActiveProfiles({ "test" })
 abstract class AppTestBootstrapper extends SpringBootTestContextBootstrapper {
 }
